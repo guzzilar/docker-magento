8e1bc1f8adf9627b3b64361148481cae801cfc27
guzzilar <iam.nuttanon@gmail.com>
2016-07-21 22:15:28 +0700

    Docker for Magento 1.9.2.4

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-docker-compose -f ./src/magento-1.9.0.0/docker-compose.yml stop
-
-docker-compose -f ./src/magento-1.9.0.0/docker-compose.yml rm -f opencart2011
-docker-compose -f ./src/magento-1.9.0.0/docker-compose.yml rm -f opencart2011_db
-
-docker-compose -f ./src/magento-1.9.0.0/docker-compose.yml up --build
