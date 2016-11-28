aff09342b91c7aed1318167c4033c0f98e8cc253
guzzilar <iam.nuttanon@gmail.com>
2016-11-29 04:17:49 +0700

    Add more command

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 target_version=""
+auto_open_browser=false
 
 while test $# -gt 0; do
     case "$1" in
@@ -8,11 +9,17 @@ while test $# -gt 0; do
             echo "1.9.2.2"
             echo "1.9.2.4"
             echo "2.1.1"
+            echo "2.1.2"
             shift
             ;;
 
         --target*)
             target_version=`echo $1 | sed -e 's/^[^=]*=//g'`
+            shift
+            ;;
+
+        --open-browser)
+            auto_open_browser=true
             shift
             ;;
 
@@ -23,13 +30,17 @@ while test $# -gt 0; do
 done
 
 build() {
-    docker-compose -f ./src/magento-$target_version/docker-compose.yml stop
-    docker-compose -f ./src/magento-$target_version/docker-compose.yml rm -f php
-    docker-compose -f ./src/magento-$target_version/docker-compose.yml rm -f mysql
+    docker-compose -f ./src/magento-$1/docker-compose.yml stop
+    docker-compose -f ./src/magento-$1/docker-compose.yml rm -f php
+    docker-compose -f ./src/magento-$1/docker-compose.yml rm -f mysql
 
-    docker-compose -f ./src/magento-$target_version/docker-compose.yml up --build
+    docker-compose -f ./src/magento-$1/docker-compose.yml up --build
+
+    [ $2 = true ] && {
+        open http://$(docker-machine ip)/magento-$1/_build
+    }
 }
 
 [[ -n $target_version ]] && { 
-    build $target_version
+    build $target_version $auto_open_browser
 }
